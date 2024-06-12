a1376275106eefa7c1acc44e3e2431f0d53247c4
MKarthik1729 <karthik.m20@iiits.in>
2024-06-12 12:14:55 +0530

    nav

---
+++ src/comp/Nav.jsx
@@ -0,0 +1,13 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <h1>NextOrder</h1>
+        {/* <FontAwesomeIcon icon="fa-solid fa-cart-shopping" /> */}
+    </div>
+  )
+}
+
+export default Nav
